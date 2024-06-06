debe356e22de05cdfdcf6fbfe7dd75133b2488b0
Kevin Guanoluisa <estebanguanoluisa55@gmail.com>
2024-06-06 18:15:18 -0500

    Sorting folders, messages and methods for messages

--- messages.js
+++ messages.js
@@ -2,23 +2,34 @@ const messages = {
   greeting:
     "😊 Aquí están algunas de las opciones que ofrecemos: \n\n" +
     "1. ✅ Información sobre nuestros productos\n" +
-    "2. 🔎 Consultas de software\n" +
+    "2. 🔎 Consultas de sobre las aplicaciones\n" +
     "3. 🗓️ Agendar una cita para una propuesta personalizada para tu proyecto.\n" +
     "4. 📝 Comentarios o sugerencias\n\n" +
     "Responde con el número de la opción que deseas o una palabra clave.",
+  information:
+    "📢 *Somos una empresa de desarrollo de software*, aquí están algunos de nuestros productos: \n\n",
+  product1:
+    "📱 *Aplicación móvil Salud Total*: Puedes encontrarla en el siguiente enlace:\n[🔗 Salud Total](https://play.google.com/store/apps/details?id=com.saludtotal.saludtotaleps&hl=es_EC&pli=1)\n\n",
+  product2:
+    "💻 *Aplicación web SunnyLand*: Puedes encontrarla en el siguiente enlace:\n[🔗 Aplicación web](https://ansimuz.itch.io/sunny-land-pixel-game-art)\n\n",
+  product3:
+    "🛡️ *Assets para juegos Fantasy Knight*: Puedes encontrarla en el siguiente enlace:\n[🔗 Fantasy Knight](https://aamatniekss.itch.io/fantasy-knight-free-pixelart-animated-character)\n",
+  product4:
+    "🗡️ *Aplicación web Tiny Swords*: Puedes encontrarla en el siguiente enlace:\n[🔗 Tiny Swords](https://pixelfrog-assets.itch.io/tiny-swords)\n\n",
+  product5:
+    "🌲 *Aplicación web Mystic Woods*: Puedes encontrarla en el siguiente enlace:\n[🔗 Mystic Woods](https://game-endeavor.itch.io/mystic-woods)\n\n",
   support:
     "Tenemos varias áreas de consulta para elegir. ¿Cuál de estos servicios te gustaría explorar?\n\n" +
     " a. 🚧 Problemas de instalación\n" +
     " b. 🔄 Actualizaciones y parches\n" +
     " c. 📚 Documentación y guías de usuario\n" +
-    "Responde con el número de la opción que deseas.",
-  install:
-    "¡Entiendo que estás experimentando problemas de instalación! Para ayudarte, te enviaré un archivo PDF con las instrucciones detalladas de instalación de nuestros productos. Por favor, espera un momento mientras lo preparo para ti.",
+    "Responde con la letra de la opción que deseas.",
+    install:
+    "¡Entiendo que estás experimentando problemas de instalación! Para ayudarte, te enviaré un archivo PDF con las instrucciones detalladas de instalación de nuestros productos. Por favor, espera un momento mientras lo preparo para ti. ⚙️📄",
   updates:
-    "Te invito a visitar nuestro sitio web donde podrás encontrar información detallada sobre las últimas actualizaciones y parches disponibles para nuestros productos. [https://wwebjs.dev/guide/creating-your-bot/#run-your-bot]",
-  ducumentsGuide:
-    "Puedes encontrar nuestra documentación y guías en: [https://wwebjs.dev/guide/creating-your-bot/#run-your-bot]",
-  information: "Somos un empresa de desarrollo de software",
+    "Te invito a visitar nuestro sitio web donde podrás encontrar información detallada sobre las últimas actualizaciones y parches disponibles para nuestros productos. 🌐🛠️\n[https://wwebjs.dev/guide/creating-your-bot/#run-your-bot]",
+  documentsGuide:
+    "Puedes encontrar nuestra documentación y guías en: 📚🔍\n[https://wwebjs.dev/guide/creating-your-bot/#run-your-bot]",
   promotionMessage:
     "🚀 ¡Descubre cómo nuestra empresa de desarrollo de software puede ayudarte a alcanzar tus objetivos! 🚀 \n\n" +
     "Somos expertos en ofrecer soluciones innovadoras y personalizadas para empresas de todos los tamaños. Ya sea que necesites desarrollar una aplicación móvil, un sitio web, o implementar soluciones empresariales, estamos aquí para ayudarte. \n\n" +
@@ -27,7 +38,9 @@ const messages = {
     "- Soporte técnico dedicado.\n" +
     "- Soluciones personalizadas según tus necesidades.\n" +
     "- Entrega a tiempo y dentro del presupuesto.\n\n" +
-    '¡No te pierdas la oportunidad de llevar tu empresa al siguiente nivel con nuestra ayuda! ¿Listo para comenzar?',
+    "¡No te pierdas la oportunidad de llevar tu empresa al siguiente nivel con nuestra ayuda! ¿Listo para comenzar?",
 };
-
+//TODO: Organizar mensajes
+//TODO: Conectar base de datos
+//TODO: Enviar mensaje de reunion con un ubicacion
 export default messages;
--- sendMessages.js
+++ sendMessages.js
@@ -1,171 +1,201 @@
-    import pkg from "whatsapp-web.js";
-    import messages from "./messages.js";
+import pkg from "whatsapp-web.js";
+import messages from "./messages.js";
+import {
+  informacionKeywords,
+  consultaKeywords,
+  agendarKeywords,
+  comentariosKeywords,
+} from "./keysWord.js";
 
-    const { MessageMedia } = pkg;
+const { MessageMedia } = pkg;
 
-    class MessageHandler {
-        constructor(client) {
-            this.client = client;
-            this.initialize();
-        }
+class MessageHandler {
+  constructor(client) {
+    this.client = client;
+    this.initialize();
+  }
 
-        initialize() {
-            this.client.on('message', this.handleMessage.bind(this));
-        }
+  initialize() {
+    this.client.on("message", this.handleMessage.bind(this));
+  }
 
-        async handleMessage(msg) {
+  validKeyWord(word, keysWords) {
+    return keysWords.some((keyWord) => word.includes(keyWord));
+  }
 
-            console.log('De: ', msg.from);
+  async handleMessage(msg) {
+    console.log("De: ", msg.from);
 
-            if (msg.from != "593979353728@c.us" || msg.from != "593979353728@c.us") {
-                console.log('Mensaje de grupo ignorado:', msg.from);
-                return; 
-            }
-
-            console.log('Mensaje: ', msg.body);
-            
-            function sleep(ms) {
-                return new Promise(resolve => setTimeout(resolve, ms));
-            }
-            
-            await sleep(10000);
-
-            const lowerCaseMessage = msg.body.toLowerCase();
-            const greetings = ['hola', 'buenos', 'buenas', 'hey', 'hi', 'hello'];
-
-            const isGreeting = greetings.some(greeting => lowerCaseMessage.includes(greeting));
-
-
-            if (isGreeting) {
-                await this.sendGreeting(msg.from);
-            } else if (lowerCaseMessage === '1') {
-                await this.sendSupport(msg.from);
-            } else if (lowerCaseMessage === '2') {
-                await this.sendInformation(msg.from);
-            } else if (lowerCaseMessage === '3') {
-                await this.sendQuery(msg.from);
-            } else if (lowerCaseMessage === '4') {
-                await this.sendFollow(msg.from);
-            } else if (lowerCaseMessage === '5') {
-                await this.sendSolicitid(msg.from);
-            } else if (lowerCaseMessage === 'a') {
-                await this.sendInstall(msg.from);
-            } else if (lowerCaseMessage === 'b') {
-                await this.sendUpdate(msg.from);
-            } else if (lowerCaseMessage == 'c'){
-                await this.sendDocuments(msg.from)
-            } else {
-                await this.sendDefaultResponse(msg);
-            }
-        }
-
-        async sendGreeting(to) {
-            await this.client.sendMessage(to, " Hola👋"+ messages.greeting)
-                .then(res => {
-                    console.log('Saludo enviado exitosamente',res.body);
-                })
-                .catch(err => {
-                    console.error('Error al enviar el saludo:', err);
-                });
-
-            const stickerPath = './Sin título.jpg'; 
-            const stickerMedia = MessageMedia.fromFilePath(stickerPath);
-
-            await this.client.sendMessage(to, stickerMedia, { sendMediaAsSticker: true })
-        }
-
-        async sendInformation(to) {
-            await this.client.sendMessage(to, messages.information)
-                .then(res => {
-                    console.log('Menú enviado exitosamente:', res.body);
-                })
-                .catch(err => {
-                    console.error('Error al enviar el menú:', err);
-                });
-        }
-
-        async sendSupport(to) {
-            await this.client.sendMessage(to, messages.support)
-                .then(res => {
-                    console.log('Pedidos enviados exitosamente:', res.body);
-                })
-                .catch(err => {
-                    console.error('Error al enviar los pedidos:', err);
-                });
-        }
-
-        async sendInstall(to) {
-            await this.client.sendMessage(to, messages.install)
-                .then(res => {
-                    console.log('Pedidos enviados exitosamente:', res.body);
-                })
-                .catch(err => {
-                    console.error('Error al enviar los pedidos:', err);
-                });
-
-                const pdfPath = './install.pdf';
-                const pdfMedia = MessageMedia.fromFilePath(pdfPath);
-            
-                await this.client.sendMessage(to, pdfMedia)
-                .then(res => {
-                    console.log('PDF enviado exitosamente');
-                })
-                .catch(err => {
-                    console.error('Error al enviar el PDF:', err);
-                });
-        }
-
-        async sendUpdate(to) {
-            await this.client.sendMessage(to, messages.updates)
-                .then(res => {
-                    console.log('Pedidos enviados exitosamente:', res.body);
-                })
-                .catch(err => {
-                    console.error('Error al enviar los pedidos:', err);
-                });
-        }
-
-        async sendDocuments(to){
-            await this.client.sendMessage(to,messages.ducumentsGuide)
-        }
-
-        
-        async sendQuery(to) {
-            const media = MessageMedia.fromFilePath("./v2.mp4")
-            await this.client.sendMessage(to,media)
-            .then(res => {
-                console.log('video enviado exitosamente');
-                })
-        }
-
-        
-        async sendFollow(to) {
-            const media = MessageMedia.fromFilePath("./v1.mp4")
-            await this.client.sendMessage(to,media)
-            .then(res => {
-                console.log('video enviado exitosamente');
-                })
-        }
-
-        async sendSolicitid(to){
-            const media = MessageMedia.fromFilePath("./v3.mp4")
-            await this.client.sendMessage(to,media)
-            .then(res => {
-                console.log('video enviado exitosamente');
-                })
-        }
-
-        async sendDefaultResponse(msg) {
-            const defaultMessage = 'Lo siento, no entendí tu mensaje. Por favor elige una opción:\n\n' + messages.greeting;
-            await this.client.sendMessage(msg.from, defaultMessage)
-                .then(res => {
-                    console.log('Respuesta predeterminada enviada exitosamente:', res.body);
-                })
-                .catch(err => {
-                    console.error('Error al enviar la respuesta predeterminada:', err);
-                });
-        }
-        
+    if (
+      msg.from != "593979353728@c.us" &&
+      msg.from != "593984635564@c.us" &&
+      msg.from != "593984493368@c.us"
+    ) {
+      console.log("Mensaje ignorado de:", msg.from);
+      return;
     }
 
-    export default MessageHandler
+    console.log("Mensaje: ", msg.body);
+
+    // function sleep(ms) {
+    //     return new Promise(resolve => setTimeout(resolve, ms));
+    // }
+
+    // await sleep(10000);
+
+    const lowerCaseMessage = msg.body.toLowerCase();
+    const greetings = ["hola", "buenos", "buenas", "hey", "hi", "hello"];
+
+    if (this.validKeyWord(lowerCaseMessage, greetings)) {
+      await this.sendGreeting(msg.from);
+    } else if (this.validKeyWord(lowerCaseMessage, informacionKeywords)) {
+      await this.sendInformation(msg.from);
+    } else if (this.validKeyWord(lowerCaseMessage, consultaKeywords)) {
+      await this.sendQuery(msg.from);
+    } else if (this.validKeyWord(lowerCaseMessage, agendarKeywords)) {
+      await this.sendAgenda(msg.from);
+    } else if (this.validKeyWord(lowerCaseMessage, comentariosKeywords)) {
+      await this.sendComment(msg.from);
+    } else if (lowerCaseMessage === "a") {
+      await this.sendInstall(msg.from);
+    } else if (lowerCaseMessage === "b") {
+      await this.sendUpdate(msg.from);
+    } else if (lowerCaseMessage == "c") {
+      await this.sendDocuments(msg.from);
+    } else {
+      await this.sendDefaultResponse(msg);
+    }
+  }
+
+  async sendGreeting(to) {
+    const logo = MessageMedia.fromFilePath("./assets/img/logo.jpg");
+    await this.client.sendMessage(to, logo);
+
+    await this.client
+      .sendMessage(to, " Hola👋" + messages.greeting)
+      .then((res) => {
+        console.log("Saludo enviado exitosamente", res.body);
+      })
+      .catch((err) => {
+        console.error("Error al enviar el saludo:", err);
+      });
+  }
+
+  async sendInformation(to) {
+    await this.client
+      .sendMessage(to, messages.information)
+      .then((res) => {
+        console.log("Mensaje enviado exitosamente:", res.body);
+      })
+      .catch((err) => {
+        console.error("Error al enviar el menú:", err);
+      });
+
+    const product1 = MessageMedia.fromFilePath("./assets/img/product1.png");
+    await this.client.sendMessage(to, product1, { caption: messages.product1 });
+
+    const product2 = MessageMedia.fromFilePath("./assets/img/product2.png");
+    await this.client.sendMessage(to, product2, { caption: messages.product2 });
+
+    const product3 = MessageMedia.fromFilePath("./assets/img/product3.gif");
+    await this.client.sendMessage(to, product3, { caption: messages.product3 });
+
+    const product4 = MessageMedia.fromFilePath("./assets/img/product4.png");
+    await this.client.sendMessage(to, product4, { caption: messages.product4 });
+
+    const product5 = MessageMedia.fromFilePath("./assets/img/product4.png");
+    await this.client.sendMessage(to, product5, { caption: messages.product4 });
+  }
+
+  async sendQuery(to) {
+    await this.client
+      .sendMessage(to, messages.support)
+      .then((res) => {
+        console.log("Mensaje enviados exitosamente:", res.body);
+      })
+      .catch((err) => {
+        console.error("Error al enviar:", err);
+      });
+
+    const media = MessageMedia.fromFilePath("./v1.mp4");
+    await this.client.sendMessage(to, media).then((res) => {
+      console.log("video enviado exitosamente");
+    });
+  }
+
+  async sendAgenda(to) {
+    
+    const media = MessageMedia.fromFilePath("./v2.mp4");
+    await this.client.sendMessage(to, media).then((res) => {
+      console.log("video enviado exitosamente");
+    });
+  }
+
+  async sendComment(to) {
+    const media = MessageMedia.fromFilePath("./v3.mp4");
+    await this.client.sendMessage(to, media).then((res) => {
+      console.log("video enviado exitosamente");
+    });
+  }
+
+  async sendInstall(to) {
+    await this.client
+      .sendMessage(to, messages.install)
+      .then((res) => {
+        console.log("Pedidos enviados exitosamente:", res.body);
+      })
+      .catch((err) => {
+        console.error("Error al enviar los pedidos:", err);
+      });
+
+    const pdfPath = "./assets/install.pdf";
+    const pdfMedia = MessageMedia.fromFilePath(pdfPath);
+
+    await this.client
+      .sendMessage(to, pdfMedia)
+      .then((res) => {
+        console.log("PDF enviado exitosamente",res.body);
+      })
+      .catch((err) => {
+        console.error("Error al enviar el PDF:", err);
+      });
+  }
+
+  async sendUpdate(to) {
+    await this.client
+      .sendMessage(to, messages.updates)
+      .then((res) => {
+        console.log("Pedidos enviados exitosamente:", res.body);
+      })
+      .catch((err) => {
+        console.error("Error al enviar los pedidos:", err);
+      });
+  }
+
+  async sendDocuments(to) {
+    await this.client.sendMessage(to, messages.ducumentsGuide);
+  }
+
+  async sendDefaultResponse(msg) {
+    const defaultMessage =
+      "Lo siento, no entendí tu mensaje. Por favor elige una opción:\n\n" +
+      messages.greeting;
+    await this.client
+      .sendMessage(msg.from, defaultMessage)
+      .then((res) => {
+        console.log("Respuesta predeterminada enviada exitosamente:", res.body);
+      })
+      .catch((err) => {
+        console.error("Error al enviar la respuesta predeterminada:", err);
+      });
+
+    const stickerPath = "./assets/img/sitcke1.jpg";
+    const stickerMedia = MessageMedia.fromFilePath(stickerPath);
+
+    await this.client.sendMessage(to, stickerMedia, {
+      sendMediaAsSticker: true,
+    });
+  }
+}
+
+export default MessageHandler;
